2e19d36d8b88b26049fdf24e2a79ceed9b84695e
Arthur Paiva <38266341+ArthurPaiva96@users.noreply.github.com>
2021-07-30 21:05:19 -0300

    Some changes in comments

--- selecaoIngenua.c
+++ selecaoIngenua.c
@@ -76,42 +76,6 @@ int main()
 	return 0;
 }
 
-/*
-
-VARIAR TAMANHO DOS GRUPOS (FIXA K E N)
-
-tamanhoDoGrupo = 7
-tamanhoDoGrupo = 9
-tamanhoDoGrupo = 11
-tamanhoDoGrupo = 13
-tamanhoDoGrupo = 15
-tamanhoDoGrupo = 17
-tamanhoDoGrupo = 19
-tamanhoDoGrupo = 21
-tamanhoDoGrupo = 23
-tamanhoDoGrupo = 25
-
-
-Trocar tamanhoDoGrupo
-
-    int tamanhoDoGrupo = 5;
-    //array de medianas
-    int numeroDeMedianas = floor((tamanhoArray+tamanhoDoGrupo-1)/tamanhoDoGrupo);
-    int *medianas = (int*) malloc(numeroDeMedianas * sizeof(int));
-
-    int i;
-    for(i = 0; i < tamanhoArray/tamanhoDoGrupo; i++){
-        medianas[i] = encontrarMediana(array+baixo+i*tamanhoDoGrupo, tamanhoDoGrupo);
-    }
-    
-    if(i*tamanhoDoGrupo < tamanhoArray){
-        medianas[i] = encontrarMediana(array+baixo+i*tamanhoDoGrupo, tamanhoArray%tamanhoDoGrupo);
-        i++;
-    }
-
-
-
-*/
 
 /*
 
@@ -173,8 +137,8 @@ FIXA N VARIA K
 
 int main()
 {
-	int array[] = {159, 376, 208, 384, 213, 85, 278, 1, 364, 389, 155, 91, 234, 279, 312, 356, 260, 347, 77, 273, 231, 170, 360, 48, 41, 386, 59, 345, 209, 145, 387, 282, 223, 16, 374, 132, 163, 228, 314, 230, 337, 21, 338, 261, 112, 334, 196, 56, 325, 6, 22, 346, 371, 73, 270, 80, 108, 370, 233, 18, 3, 58, 255, 262, 122, 184, 305, 277, 365, 287, 134, 173, 70, 379, 311, 359, 315, 55, 326, 197, 125, 31, 247, 182, 104, 23, 60, 162, 141, 304, 399, 194, 333, 271, 36, 107, 2, 181, 83, 350};
-    int tamanhoArray = sizeof(array)/sizeof(array[0]);
+	int vetor[] = {159, 376, 208, 384, 213, 85, 278, 1, 364, 389, 155, 91, 234, 279, 312, 356, 260, 347, 77, 273, 231, 170, 360, 48, 41, 386, 59, 345, 209, 145, 387, 282, 223, 16, 374, 132, 163, 228, 314, 230, 337, 21, 338, 261, 112, 334, 196, 56, 325, 6, 22, 346, 371, 73, 270, 80, 108, 370, 233, 18, 3, 58, 255, 262, 122, 184, 305, 277, 365, 287, 134, 173, 70, 379, 311, 359, 315, 55, 326, 197, 125, 31, 247, 182, 104, 23, 60, 162, 141, 304, 399, 194, 333, 271, 36, 107, 2, 181, 83, 350};
+    int tam = sizeof(vetor)/sizeof(vetor[0]);
 
     double s_CPU_inicial = 0, s_total_inicial = 0, s_CPU_final = 0, s_total_final = 0;
 
@@ -182,12 +146,12 @@ int main()
 
         Tempo_CPU_Sistema(&s_CPU_inicial, &s_total_inicial);
 
-        int kMenor = selecao(array, 0, tamanhoArray-1, k);
+        int k_menor = select_ingenuo(vetor, 0, tam-1, k-1);
     
         Tempo_CPU_Sistema(&s_CPU_final, &s_total_final);
 
         printf ("Tempo de CPU total = %f\n", s_CPU_final - s_CPU_inicial);
-        printf("O k-menor é: %d \n", kMenor);
+        printf("O k-menor é: %d \n", k_menor);
     }
 
     
